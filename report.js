febb95ace28f876fe85a687eafdcdc77281caa42
Denvab <denvabgraveiron@gmail.com>
2024-08-06 10:13:00 +0100

    Fixed asyncing issue when inserting values into table

--- report.js
+++ report.js
@@ -1,34 +1,22 @@
-import mysql2 from 'mysql2'
 const require = createRequire(import.meta.url);
 import { createRequire } from "module";
 const temp = require('mysql2/promise')
 
 function GenerateReport(pages, url) {
+    // Formats hostname so it can be used to name and refernce our table
     const formatedURL = new URL(url).hostname.split('.')
     const tableName = `tbl_${formatedURL[0]}dot${formatedURL[1]}`
     // Even if order is not important, is needed to format list correctly for the loop
     const sortedPages = sortPages(pages);
 
-    // Creates database & table if doesn't already exist
     connectToDatabase();
     createDatabase();
     connectToDatabase('linksdb');
-    // Makes sure table is truncated before any new values are inserted, if table already existed
-    createTable(tableName)
-
-    //createTable(tableName).then(fillTable(sortedPages, tableName))
+    createTable(tableName, sortedPages, tableName);
 
     // Inserts results into table of scraped website
-    fillTable(sortedPages, tableName)
-
-    console.log(`Table filled`);
+    //fillTable(sortedPages, tableName);
 };
-
-function fillTable(pages,table) {
-    for (const page of pages) {
-        pool.query(`INSERT INTO ${table}(link,count) VALUES(${escape(page[0])}, ${page[1]})`, function(error) {if (error) throw error; console.log(`Record inserted`)})
-    };
-}
 
 // Connects program to MySQL
 let pool
@@ -39,17 +27,35 @@ function connectToDatabase(database) {
         password: '',
         database: database
     });
-}
-
-function createDatabase(){
-    pool.query('CREATE DATABASE IF NOT EXISTS linksdb', function(error, result) {if (error) throw error; console.log(`Database created`)});
 };
 
-function createTable(table) {
-    pool.query(`CREATE TABLE IF NOT EXISTS ${table} (id INT AUTO_INCREMENT PRIMARY KEY, link TEXT, count INT)`,
-    function(error, result) {if (error) throw error; console.log(`Table created`)});
+function createDatabase(){
+    try {
+        pool.query('CREATE DATABASE IF NOT EXISTS linksdb');
+        console.log(`Database created`);
+    } catch (error) {
+        throw new Error(error.message);
+    };
+};
 
-    pool.query(`TRUNCATE TABLE ${table}`, function() {console.log(`truncate`)})
+async function createTable(table, pages) {
+    // Creates our website if it doesn't exist already, then clears its content in the case it already existed with entries
+    try {
+        await pool.query(`CREATE TABLE IF NOT EXISTS ${table} (id INT AUTO_INCREMENT PRIMARY KEY, link TEXT, count INT)`);
+        await pool.query(`TRUNCATE TABLE ${table}`);
+        console.log(`Table created`);
+    } catch (error) {
+        throw new Error(error.message);
+    };
+
+    // Concurrently fills the table with entries. Wont be added in sorted order, use ORDER BY query for that
+    // Must be in this function. If seporate funcation, it will cause ascyning issues
+    try {
+        await Promise.all(pages.map((page) =>pool.query(`INSERT INTO ${table}(link,count) VALUES(${temp.escape(page[0])}, ${page[1]})`)));
+        console.log(`Table filled`);
+    } catch (error) {
+        console.log(error.message);
+    };
 };
 
 // Defunct and should only be used manually if can't to MySQL
